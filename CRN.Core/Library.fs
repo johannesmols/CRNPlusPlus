4f63f0ef9ba81ec92a5b3bf3a07442e84d870d3e
Johannes Mols <johannes.mols@googlemail.com>
2022-06-14 12:11:03 +0200

    Added types, Fparsec package, start of basic parser

---
+++ CRN.Core/Library.fs
@@ -0,0 +1,62 @@
+﻿module CRN.Core
+
+open FParsec
+
+type Literal =
+    | SpeciesLiteral of string
+    | IntLiteral of int
+    | FloatLiteral of float
+
+type ModuleStmt =
+    | Load of Literal * Literal
+    | Add of Literal * Literal * Literal
+    | Subtract of Literal * Literal * Literal
+    | Multiply of Literal * Literal * Literal
+    | Divide of Literal * Literal * Literal
+    | SquareRoot of Literal * Literal
+    | Compare of Literal * Literal
+    
+type ConditionalStmt =
+    | IfGreaterThan of Command list
+    | IfGreaterThanOrEquals of Command list
+    | IfEquals of Command list
+    | IfLesserThan of Command list
+    | IfLesserThanOrEquals of Command list
+    
+and Command =
+    | ArithmeticStmt // no definition ??
+    | ComparisonStmt // no definition ??
+    | ConditionalStmt of ConditionalStmt
+    | ModuleStmt of ModuleStmt
+
+type Statements =
+    | ConcentrationStmt of Literal * Literal
+    | StepStmt of Command list 
+
+type Crn = {
+    Statements : Statements list
+}
+
+let ws : Parser<_, unit> = skipMany (skipChar ' ')
+
+let intOrFloatLiteral =
+    numberLiteral (NumberLiteralOptions.DefaultFloat ||| NumberLiteralOptions.DefaultInteger) "number"
+    |>> fun n ->
+            if n.IsInteger then Literal.IntLiteral (int n.String)
+            else Literal.FloatLiteral (float n.String)
+    .>> ws
+
+let speciesLiteral = many1Chars (letter <|> digit) |>> Literal.SpeciesLiteral .>> ws
+
+let statements : Parser<_, unit> = spaces // TODO
+
+let program = skipString "crn"
+              >>. spaces
+              >>. skipChar '='
+              >>. spaces
+              >>. skipChar '{'
+              >>. spaces
+              >>. statements
+              >>. spaces
+              >>. skipString "};" 
+let programFull = spaces >>. program .>> spaces .>> eof
--- CRN/Program.fs
+++ CRN/Program.fs
@@ -1,4 +1,30 @@
+open CRN.Core
 
+open FParsec
 
-// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+let input = """
+crn = {
+    conc[a, a0],
+    conc[b, b0],
+    step[{
+        ld[a, atmp],
+        ld[b, btmp],
+        cmp[a, b]
+    }],
+    step[{
+        ifGT[{ sub[atmp, btmp, a] }],
+        ifLT[{ sub[btmp, atmp, b] }]
+    }]
+};
+"""
+
+let parse input =
+    match run programFull input with
+    | Success(res, _, _) -> Result.Ok res
+    | Failure(err, _, _) -> Result.Error err
+    
+let res = parse input
+
+match res with
+| Result.Ok res -> printfn $"Success: {res}"
+| Result.Error err -> printfn $"Failure: {err}"
