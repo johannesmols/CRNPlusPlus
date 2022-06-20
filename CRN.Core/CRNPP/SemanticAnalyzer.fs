0efed275874e770abb036119c5992d4f26bdd6ee
Johannes Mols <johannes.mols@googlemail.com>
2022-06-20 11:45:06 +0200

    Part 2 of analysing, but not doing it because paper is violating it itself

--- CRN.Core/CRNPP/SemanticAnalyzer.fs
+++ CRN.Core/CRNPP/SemanticAnalyzer.fs
@@ -4,6 +4,13 @@ module CRN.Core.CRNPP.SemanticAnalyzer
 
 open CRN.Core.CRNPP.Types
 
+/// Extract only the step statements from a program
+let getStepStatements crn =
+    crn.Statements |> List.choose (fun s ->
+        match s with
+        | ConcentrationStmt _ -> None
+        | StepStmt cmds -> Some cmds)
+
 /// Get a list of species names where the value has not been defined in the program
 let extractMissingArguments crn =
     crn.Statements
@@ -62,23 +69,46 @@ let statementsAreNotConflicting crn =
                 | IfGreaterThan c | IfGreaterThanOrEquals c | IfEquals c | IfLesserThan c | IfLesserThanOrEquals c ->
                     c |> List.forall cmdContainsComparison
         
-        crn.Statements
-        |> List.choose (fun s ->
-            match s with
-            | ConcentrationStmt _ -> None
-            | StepStmt cmds -> Some cmds)
+        getStepStatements crn
         |> List.forall (fun cmds ->
             let hasComparison = cmds |> List.exists cmdContainsComparison
             let hasConditional = cmds |> List.exists (fun c -> match c with | ConditionalStmt _ -> true | _ -> false)
             not(hasComparison && hasConditional))
         
     let noReadingAndWritingToSameSpecies crn =
-        true
+        let rec constructReadWriteTable (read: string list) (write: string list) = function
+            | [] -> read, write
+            | cmd::rest ->
+                match cmd with
+                | ConditionalStmt stmt ->
+                    match stmt with
+                    | IfGreaterThan c | IfGreaterThanOrEquals c | IfEquals c | IfLesserThan c | IfLesserThanOrEquals c ->
+                        constructReadWriteTable read write c
+                        |> fun (r, w) -> constructReadWriteTable r w rest
+                | ModuleStmt stmt ->
+                    match stmt with
+                    | Load(SpeciesLiteral i, SpeciesLiteral t)
+                    | SquareRoot(SpeciesLiteral i, SpeciesLiteral t) -> i::read, t::write
+                    | Add(SpeciesLiteral i1, SpeciesLiteral i2, SpeciesLiteral t)
+                    | Subtract(SpeciesLiteral i1, SpeciesLiteral i2, SpeciesLiteral t)
+                    | Multiply(SpeciesLiteral i1, SpeciesLiteral i2, SpeciesLiteral t)
+                    | Divide(SpeciesLiteral i1, SpeciesLiteral i2, SpeciesLiteral t) -> i1::i2::read, t::write
+                    | Compare(SpeciesLiteral i, SpeciesLiteral t) -> i::t::read, write
+                    | _ -> failwith "Attempted to use float literals in module statements."
+                    |> fun (r, w) -> constructReadWriteTable r w rest
+        
+        getStepStatements crn
+        |> List.map (fun cmds -> cmds |> constructReadWriteTable [] [])
+        |> List.forall (fun (r, w) ->
+            w |> List.distinct |> List.length = w.Length && // Not writing to same variables multiple times
+            // TODO: Does not work because conditional statements are flattened. Two conditionals might write to the same species, but only one is executed.
+            (Set.ofList r, Set.ofList w) ||> Set.intersect = Set.empty) // Read and write actions don't use same variables
+            // TODO: Does not work because due to same reason. Also, programs like Pi violate the rule stated in the paper (reading and writing to same species)
     
     if not(comparisonAndConditionalExecutionAreSeparated crn) then
         Result.Error "Comparison module and conditional statements cannot be used in the same step"
-    else if not(noReadingAndWritingToSameSpecies crn) then
-        Result.Error "Can't read and write to the same species in the same step"
+    //else if not(noReadingAndWritingToSameSpecies crn) then
+    //    Result.Error "Can't read and write to the same species in the same step."
     else
         Result.Ok crn
 
@@ -92,7 +122,7 @@ let analyze (crn: Crn) : Result<Crn, string> =
         Result.Error "All concentration declarations must happen before any steps."
     else if not(moduleOutputSpeciesAreDifferentFromInput crn) then
         Result.Error "Module statements must not have an output that is the same as an input."
-    else if (match conflictingResult with | Error err -> true | Ok _ -> false) then
+    else if (match conflictingResult with | Error _ -> true | Ok _ -> false) then
         conflictingResult
     else
         Result.Ok crn
